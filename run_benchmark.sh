cea3bc9fc7f96295e79ef00c97ea91cbf9ef9050
alberto <alberto@alberto-VirtualBox.(none)>
2013-09-27 22:52:44 -0600

    Updating project

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -9,7 +9,7 @@ BENCH_ARG="${BENCH_INP} ${BENCH_OPT} ${BENCH_OUT}"
 #
 i=0
 
-#echo "Data collection for MPEG2 experiment" > total_log.txt
+echo "Data collection for MPEG2 experiment" > total_log.txt
 
 for bpredm in comb 2lev
 do
@@ -18,16 +18,16 @@ do
       for ruus in 16 32
       do
          ${WATTCH_BIN} -bpred ${bpredm} -res:fpalu 1 -lsq:size ${lsqs} -ruu:size ${ruus}  ${BENCH_BIN} ${BENCH_ARG} > tmp_log.txt 2>&1
-         echo "\nData for run $((i+=1))" >> total_log.txt
+        echo "\nData for run $((i+=1)), -bpred ${bpredm} -res:fpalu 1 -lsq:size ${lsqs} -ruu:size ${ruus} cache mode l" >> total_log.txt
          grep "Total Power Consumption" tmp_log.txt >> total_log.txt
          grep "sim_IPC" tmp_log.txt >> total_log.txt
          grep "sim_CPI" tmp_log.txt >> total_log.txt
          grep "\.misses " tmp_log.txt | grep -v "bpred" >> total_log.txt
          grep "\.hits" tmp_log.txt >> total_log.txt
          grep "\.miss_rate" tmp_log.txt >> total_log.txt
-      done
+     done
       ${WATTCH_BIN} -bpred ${bpredm} -res:fpalu 1 -lsq:size ${lsqs} -ruu:size 32 -cache:dl1 dl1:128:32:4:f -cache:dl2 ul2:1024:64:4:f -cache:il1 il1:512:32:1:f ${BENCH_BIN} ${BENCH_ARG} > tmp_log.txt 2>&1
-      echo "\nData for run $((i+=1))" >> total_log.txt
+      echo "\nData for run $((i+=1)), -bpred ${bpredm} -res:fpalu 1 -lsq:size ${lsqs} -ruu:size 32  cache mode f" >> total_log.txt
       grep "Total Power Consumption" tmp_log.txt >> total_log.txt
       grep "sim_IPC" tmp_log.txt >> total_log.txt
       grep "sim_CPI" tmp_log.txt >> total_log.txt
